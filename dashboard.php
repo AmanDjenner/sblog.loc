f4e4f8109219a6474928748a243b6352dd7c6363
Gheorghi Verdes <gheorghi.verdes@gmail.com>
2019-12-07 19:16:31 +0200

    Admin Design

---
+++ dashboard.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Bootstra[</title>
+    <link rel="stylesheet" href="/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/css/adminstyles.css">
+
+    </ head>
+
+<body>
+   <div class="container-fluid">
+        <div class="row">
+            <div class="col-sm-2">
+                <h1>Adminka</h1>
+
+                <ul >
+                    <li class=" active dropdown-item"><a  href="dashboard.php">Dashboard</a></li>
+                    <li class="dropdown-item"><a  href="#">Post nou</a></li>
+                    <li class="dropdown-item"><a  href="#">Categorii</a></li>
+                    <li class="dropdown-item"><a  href="#"></a></li>
+                    <li class="dropdown-item"><a  href="#">Administrator</a></li>
+                    <li class="dropdown-item"><a  href="#">Comentarii</a></li>
+                    <li class="dropdown-item"><a  href="#">Live Blog</a></li>
+                    <li class="dropdown-item"><a  href="#">Iesire</a></li>
+                </ul>
+
+             </div><!-- end side areea -->
+            <div class="col-sm-10">
+                <h4>About</h4>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita animi adipisci recusandae asperiores eveniet velit cum natus cupiditate. Reiciendis iste earum id dolores voluptate beatae error vero et dolorem modi.</p>
+                <h4>About</h4>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita animi adipisci recusandae asperiores eveniet velit cum natus cupiditate. Reiciendis iste earum id dolores voluptate beatae error vero et dolorem modi.</p>
+                <h4>About</h4>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita animi adipisci recusandae asperiores eveniet velit cum natus cupiditate. Reiciendis iste earum id dolores voluptate beatae error vero et dolorem modi.</p>
+                <h4>About</h4>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita animi adipisci recusandae asperiores eveniet velit cum natus cupiditate. Reiciendis iste earum id dolores voluptate beatae error vero et dolorem modi.</p>
+                <h4>About</h4>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita animi adipisci recusandae asperiores eveniet velit cum natus cupiditate. Reiciendis iste earum id dolores voluptate beatae error vero et dolorem modi.</p>
+                <h4>About</h4>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita animi adipisci recusandae asperiores eveniet velit cum natus cupiditate. Reiciendis iste earum id dolores voluptate beatae error vero et dolorem modi.</p>
+            </div><!-- end main areea -->
+        </div><!-- container-fluid -->
+   </div>
+
+
+
+
+    <script src="/js/jquery-3.4.1.min.js"></script>
+    <script src="/js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
